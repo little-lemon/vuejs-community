3ef677c32ff50361254f5aca66ca8c73ba00f0ac
林英英 <766019882@qq.com>
2019-01-18 14:45:51 +0800

    退出用户登录

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,4 +1,15 @@
 import Vue from 'vue'
 import validator from './validator'
+import dropdown from './dropdown'
 
-Vue.directive('validator', validator)
+const directives = {
+  validator,
+  dropdown
+}
+
+console.log(directives)
+console.log(Object.entries(directives))
+
+for (const [key, value] of Object.entries(directives)) {
+  Vue.directive(key, value)
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './directives'
 import './components'
 import store from './store'
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+
+// 使用插件
+Vue.use(VueSweetalert2)
 
 Vue.config.productionTip = false
 
@@ -13,7 +16,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  // 注入 store
   store,
   components: { App },
   template: '<App/>'
